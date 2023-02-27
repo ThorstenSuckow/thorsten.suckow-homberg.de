61fbcb8eac321b83c1afda3dbd3ef95ff6402bf2
ThorstenSuckow <thorsten@suckow-homberg.de>
2023-02-27 22:30:02 +0100

    refactor: update github links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,14 +41,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/thorstensuckow/thorsten.suckow-homberg.de/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/thorstensuckow/thorsten.suckow-homberg.de/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/thorstensuckow/thorsten.suckow-homberg.de/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/thorstensuckow/thorsten.suckow-homberg.de/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
