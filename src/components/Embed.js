3e4c910a07f423453e1a88a6df8526a162dda151
ThorstenSuckow <thorsten@suckow-homberg.de>
2023-03-18 16:37:15 +0100

    refactor: remove obsolete ref

--- src/components/Embed.js
+++ src/components/Embed.js
@@ -7,10 +7,8 @@ import Gist from "react-gist";
 
 function Embed ({width, children, title, figure, modeAware}) {
 
-    const ref = React.createRef();
-
     return (
-        <div style={{width, textAlign:"center",margin:20}} ref={ref}>
+        <div style={{width, textAlign:"center",margin:20}}>
             <div className={modeAware !== false ? "umlImg" : ""}>{children}</div>
             <div style={{textAlign:"center"}}>
                 <sup ><b>{l8.isString(figure) ? figure : `Figure ${figure ?? 1}`}</b> <span dangerouslySetInnerHTML={{__html: title}}></span></sup>
