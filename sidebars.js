88b14c89b495826b49d8b68ca6f73334c2f3a61b
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2023-05-07 23:41:13 +0200

    docs(articles): add draft for attributed property accessor article

--- sidebars.js
+++ sidebars.js
@@ -8,6 +8,7 @@ const sidebars = {
             link: {type: 'doc', id: 'articles/index'},
             items: [
                 "articles/software-craftsmanship/index",
+                "articles/php8-constructor-property-promotion-attributed-accessors/index",
                 {
                     type: 'category',
                     label: 'The Payroll System',
