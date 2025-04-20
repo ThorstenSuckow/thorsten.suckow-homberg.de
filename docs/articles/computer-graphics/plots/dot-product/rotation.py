af38b818bc9b2f13169b38cc4a90bb047daaadff
Thorsten Suckow-Homberg <thorsten@suckow-homberg.de>
2025-04-20 23:59:03 +0200

    feat: update rotation plot

--- docs/articles/computer-graphics/plots/dot-product/rotation.py
+++ docs/articles/computer-graphics/plots/dot-product/rotation.py
@@ -38,11 +38,11 @@ ax.quiver(0, 0, v[0], v[1], angles='xy', scale_units='xy', scale=1, color='red')
 ax.text(v[0]/2 + 0.2, v[1]/2, r'$\vec{v}$', color='red', fontsize=12)
 
 ax.quiver(0, 0, p[0], p[1], angles='xy', scale_units='xy', scale=1, color='green')
-ax.text(p[0]/2 + 0.2, p[1]/2 +0.16, r'$\vec{p} \cdot |\vec{v}|$', color='green', fontsize=12)
+ax.text(p[0]/2 + 0.2, p[1]/2 +0.16, r'$\vec{p} = \hat{p} \cdot |\vec{v}|$', color='green', fontsize=12)
 
 # unit vector directions
 ax.quiver(0, 0, p_u[0], p_u[1], angles='xy', scale_units='xy', scale=1, color='orange')
-ax.text(p_u[0]/2 + 0.4, p_u[1] + 0.12 , r'$\vec{p}$', color='orange', fontsize=12)
+ax.text(p_u[0]/2 + 0.4, p_u[1] + 0.12 , r'$\hat{p}$', color='orange', fontsize=12)
 
 ax.quiver(0, 0, v_u[0], v_u[1], angles='xy', scale_units='xy', scale=1, color='blue')
 ax.text(v_u[0]/2 - 0.2, v_u[1] , r'$\hat{v}$', color='blue', fontsize=12)
@@ -55,6 +55,10 @@ arc = Arc((0, 0), arc_radius, arc_radius,
           edgecolor='orange')
 ax.add_patch(arc)
 
+label_angle = theta_v + (theta_p - theta_v) / 2
+label_x = arc_radius * math.cos(label_angle)
+label_y = arc_radius * math.sin(label_angle)
+
 arc_radius = 1.2
 arc = Arc((0, 0), arc_radius * 2, arc_radius * 2,
           angle=0,
@@ -64,9 +68,7 @@ arc = Arc((0, 0), arc_radius * 2, arc_radius * 2,
 ax.add_patch(arc)
 ax.text(label_x - 0.3, label_y - 0.1, r'$\theta$', fontsize=14, color='red')
 
-label_angle = theta_v + (theta_p - theta_v) / 2
-label_x = arc_radius * math.cos(label_angle)
-label_y = arc_radius * math.sin(label_angle)
+
 ax.text(label_x - 0.7, label_y + 0.4, r'$\alpha$', fontsize=14, color='orange')
 
 plt.show()
